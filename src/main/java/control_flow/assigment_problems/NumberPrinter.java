fdc04898b7ee344a25218c91557a4878497c4889
Argha-devz <arghabwn2006@gmail.com>
2026-08-24 23:35:46 +0530

    Add Week 3 Category C assignment files

---
+++ src/main/java/control_flow/assigment_problems/NumberPrinter.java
@@ -0,0 +1,15 @@
+package control_flow.assigment_problems;
+
+public class NumberPrinter {
+    public static void printNumbersUpToN(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printNumbersUpToN(5);
+    }
+}
+```[cite: 3]
