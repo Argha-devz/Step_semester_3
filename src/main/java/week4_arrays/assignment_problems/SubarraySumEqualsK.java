933bf9e5ca18dc785e923a1bd44d0fc20f9d4f77
Argha-devz <arghabwn2006@gmail.com>
2026-09-04 23:16:54 +0530

    Added Week 4 assignment problems

---
+++ src/main/java/week4_arrays/assignment_problems/SubarraySumEqualsK.java
@@ -0,0 +1,31 @@
+package arrays.assigment_problems;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int currentSum = 0;
+        HashMap<Integer, Integer> prefixSumMap = new HashMap<>();
+
+        prefixSumMap.put(0, 1);
+
+        for (int num : nums) {
+            currentSum += num;
+
+            if (prefixSumMap.containsKey(currentSum - k)) {
+                count += prefixSumMap.get(currentSum - k);
+            }
+
+            prefixSumMap.put(currentSum, prefixSumMap.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int k = 2;
+        System.out.println("Subarray Count: " + subarraySum(nums, k));
+    }
+}
